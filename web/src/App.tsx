6023718188428cc8ab882084fa3b6edd3f7ebb89
Danillo Ferreira <nilloferreiira@gmail.com>
2026-05-20 03:39:01 -0300

    feat: wire React Router with all pages and layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,25 @@
-export default function App() {
-	return (
-		<div className="bg-red-500">
-			<h1>Hello World</h1>
-		</div>
-	)
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { PrivateRoute } from './components/private-route'
+import { AppLayout } from './components/layout/app-layout'
+import { LoginPage } from './pages/login-page'
+import { ConnectionsPage } from './pages/connections-page'
+import { ContactsPage } from './pages/contacts-page'
+import { MessagesPage } from './pages/messages-page'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="/connections" element={<ConnectionsPage />} />
+            <Route path="/connections/:connectionId/contacts" element={<ContactsPage />} />
+            <Route path="/connections/:connectionId/messages" element={<MessagesPage />} />
+          </Route>
+        </Route>
+        <Route path="/" element={<Navigate to="/connections" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }

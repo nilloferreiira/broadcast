0e6fa5656b6871ef6f2402599cb3c474bceed3c9
Danillo Ferreira <nilloferreiira@gmail.com>
2026-05-20 02:48:03 -0300

    feat: add Firebase emulator configuration for auth and firestore

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { connectAuthEmulator, getAuth } from "firebase/auth"
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +21,9 @@ const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+
+if (import.meta.env.VITE_USE_EMULATOR === "true") {
+	console.log("Using Firebase Emulator")
+	connectAuthEmulator(auth, "http://localhost:9099")
+	connectFirestoreEmulator(db, "localhost", 8080)
+}
